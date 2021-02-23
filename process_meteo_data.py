c5b8cb6e2d09ce93738ad99fe15174bde6d4f4ec
mintaka <mintaka@post.cz>
2021-02-23 19:44:15 +0100

    Connect to database, create table, cycle over csv files

--- process_meteo_data.py
+++ process_meteo_data.py
@@ -1,5 +1,19 @@
 import requests
 import zipfile
+import pathlib
+import sqlite3
+
+connection = sqlite3.connect("mydatabase.sqlite")
+cursor = connection.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS temperatures (
+        id INTEGER PRIMARY KEY,
+        year int,
+        month int,
+        day int,
+        temperatue real
+    );""")
+
 
 url = "https://www.chmi.cz/files/portal/docs/meteo/ok/denni_data/T-AVG/Plzensky/L1PLMI01_T_N.csv.zip"
 # TODO ziskat nazev souboru pomoci pathlib
@@ -10,5 +24,20 @@ open(filename, "wb").write(r.content)
 
 with zipfile.ZipFile(filename, "r") as zip_ref:
     zip_ref.extractall(".")
+
+csv_files = (list(pathlib.Path('.').glob('*.csv')))
+for csv_file in csv_files:
+    print(csv_file)
+
+
+"""
+with open(csv_file, encoding='cp1250') as csv_data:
+        csvreader = csv.reader(csv_data, delimiter=';')
+        for row in csvreader:
+            print(row)
+            if len(row) < 4:
+                continue
+            cursor.execute(f'INSERT INTO temperatures VALUES (null, "{row[0]}", "{row[1]}", "{row[2]}", "{row[3]}")')
+"""
 
 
