df95c1199bad615cf8aae7b9174fc1bab25cda30
mintaka <mintaka@post.cz>
2021-02-23 19:28:07 +0100

    Unpack zip datafile

--- process_meteo_data.py
+++ process_meteo_data.py
@@ -1,8 +1,14 @@
 import requests
+import zipfile
 
 url = "https://www.chmi.cz/files/portal/docs/meteo/ok/denni_data/T-AVG/Plzensky/L1PLMI01_T_N.csv.zip"
-r = requests.get(url, allow_redirects=True)
-open("L1PLMI01_T_N.csv.zip", "wb").write(r.content)
+# TODO ziskat nazev souboru pomoci pathlib
+filename = url.split("/")[-1]
 
+r = requests.get(url, allow_redirects=True)
+open(filename, "wb").write(r.content)
+
+with zipfile.ZipFile(filename, "r") as zip_ref:
+    zip_ref.extractall(".")
 
 
