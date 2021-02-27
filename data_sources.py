4cfabae66114b21170449469671fdcf4fcec5940
mintaka <mintaka@post.cz>
2021-02-27 15:50:24 +0100

    Add data Plzensky 1 a 2

--- data_sources.py
+++ data_sources.py
@@ -3,6 +3,8 @@
 average_temperature_prefix = "https://www.chmi.cz/files/portal/docs/meteo/ok/denni_data/T-AVG/"
 source_files = {
                  "Plzensky": [
+                              "L1DOMA01_T_N.csv.zip",
+                              "L1LIBK01_T_N.csv.zip",
                               "L1PLMI01_T_N.csv.zip",
                              ],
                }
