f5a725236ab3589d1d2f50714a5654da00bac6ac
Jana Koukalova <koukalova.jana@gmail.com>
2021-03-09 00:30:44 +0000

    Add data Olomocky 1 a 2 and space

--- data_sources.py
+++ data_sources.py
@@ -28,19 +28,24 @@ source_files = {
                               "L1DOMA01_T_N.csv.zip",
                               "L1LIBK01_T_N.csv.zip",
                               "L1PLMI01_T_N.csv.zip",
-                              "C1SUSI01_T_N.csv.zip",
+			                  "C1SUSI01_T_N.csv.zip",
                               "C1KHOR01_T_N.csv.zip",
-                              ],
+                             ],
                  "Ustecky": [
-                              "U1SNEZ01_T_N.csv.zip",
-                              "U2VARN01_T_N.csv.zip",
-                              ],
+                            "U1SNEZ01_T_N.csv.zip",
+                            "U2VARN01_T_N.csv.zip",
+                            ],
                  "Pardubicky": [
-                              "H3CHRU01_T_N.csv.zip",
-                              "H3KRAS01_T_N.csv.zip",
-                              ],
+                            "H3CHRU01_T_N.csv.zip",
+                            "H3KRAS01_T_N.csv.zip",
+                           ],
                  "Kralovehradecky": [
-                              "H3SVOD01_T_N.csv.zip",
-                              "H3NBYD01_T_N.csv.zip",
-                              ],
+                            "H3SVOD01_T_N.csv.zip",
+                            "H3NBYD01_T_N.csv.zip",
+                           ],
+                
+                "Olomoucky": [
+                            "O7BEAD01_T_N.csv.zip",
+                            "O7CERS01_T_N.csv.zip",
+                           ],
                }
