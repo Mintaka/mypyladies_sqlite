f8fed9a56eef772df4cc0688619a1fea1b5c9465
Tomáš Zítka <tozitka@gmail.com>
2021-03-17 11:32:40 +0100

    Add functions for plotting data by years and by stations.

--- plot_meteo_data.py
+++ plot_meteo_data.py
@@ -1,18 +1,60 @@
-from pathlib import Path
-import sqlite3
-
+import numpy as np
 from matplotlib import pyplot as plt
 
 from process_meteo_data import setup_db_connection, EXPORTS_FOLDER
 
-connection, cursor = setup_db_connection()
 
-data = cursor.execute(f'SELECT * FROM temperatures LIMIT 100;').fetchall()
 
-_, date, temp = list(zip(*data))
+def plot_temp_by_meteostation(cursor, meteostation_id, start=1970, end=1980):
+    name = cursor.execute(f'SELECT name FROM meteostations '
+                          f'WHERE id == {meteostation_id};').fetchone()[0]
 
-plt.plot(temp)
+    # MAGIC
+    colors = plt.cm.viridis(np.linspace(0, 1, end-start + 1))
+    # END of MAGIC
+    for ii, year in enumerate(range(start, end + 1)):
+        data = cursor.execute(f'SELECT * FROM temperatures '
+                              f'WHERE meteostation_id == {meteostation_id} '
+                              f"and date LIKE '{year}%'").fetchall()
+        if len(data) == 0:
+            print(f"Data missing for {name} {year}")
+            continue
 
-plt.savefig(EXPORTS_FOLDER + '/' + "temp.png")
+        _, _meteostation_id, date, temp = list(zip(*data))
 
-plt.show()
+        plt.plot(temp, label=year, color=colors[ii])
+
+    plt.legend()
+
+    plt.savefig(EXPORTS_FOLDER + '/' + f"temp_{name}_meteostation.png")
+
+
+def plot_temp_by_year(cursor, year):
+    for meteostation_id in range(1, 26):
+        data = cursor.execute(f'SELECT * FROM temperatures '
+                              f'WHERE meteostation_id == {meteostation_id} '
+                              f"and date LIKE '{year}%'").fetchall()
+
+        name = cursor.execute(f'SELECT name FROM meteostations '
+                              f'WHERE id == {meteostation_id};').fetchone()[0]
+        if len(data) == 0:
+            print(f"Data missing for {name} {year}")
+            continue
+
+        _, meteostation_id, date, temp = list(zip(*data))
+
+        plt.plot(temp, label=name)
+
+    plt.legend()
+
+    plt.savefig(EXPORTS_FOLDER + '/' + f"temp_{year}_meteostations.png")
+
+
+if __name__ == '__main__':
+    connection, cursor = setup_db_connection()
+    # for year in range(1970, 2000):
+    #     plt.figure()
+    #     plot_temp_by_year(cursor, year)
+
+    plot_temp_by_meteostation(cursor, 15, start=2004, end=2019)
+    plt.show()
