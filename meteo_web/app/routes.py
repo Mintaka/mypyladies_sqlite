f5b87c5d48f619d6b5c90c5f91ef62da2bfe73c6
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-03-27 20:17:13 +0100

    first using html

--- meteo_web/app/routes.py
+++ meteo_web/app/routes.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 from app import app
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
-
+    return '''
+<html>
+    <head>
+        <title>Pyladies</title>
+    </head>
+    <body>
+        <h1>Hello Pyladies!</h1>
+    </body>
+</html>'''
 
