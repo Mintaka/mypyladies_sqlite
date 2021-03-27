8a01e4fc51f809059cc715845556021102aaff35
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-03-27 21:24:40 +0100

    display metostation data

--- meteo_web/app/routes.py
+++ meteo_web/app/routes.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from app import app
 from flask import render_template
+from app.models import *
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    user = 'Liba'
-    return render_template('index.html', title="MyMeteo", user=user)
+    meteost_all = meteostations.query.order_by(meteostations.name).all()
+    return render_template('index.html', title="MyMeteo", meteost_all=meteost_all)
 
