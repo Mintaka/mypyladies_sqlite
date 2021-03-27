a144549abe0baf89e7d3c10e67535a244d9e69f3
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-03-27 20:27:11 +0100

    using html in template

--- meteo_web/app/routes.py
+++ meteo_web/app/routes.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
 from app import app
-
+from flask import render_template
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return '''
-<html>
-    <head>
-        <title>Pyladies</title>
-    </head>
-    <body>
-        <h1>Hello Pyladies!</h1>
-    </body>
-</html>'''
+    return render_template('index.html')
 
