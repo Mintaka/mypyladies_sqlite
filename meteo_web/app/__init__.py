d8df53af96e6b76657fc077a379c847e75effda0
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-03-27 22:14:38 +0100

    added template html5 UP

--- meteo_web/app/__init__.py
+++ meteo_web/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="assets")
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
