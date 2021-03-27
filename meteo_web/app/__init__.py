ec6b444fc64189c571c5d860a046addfeca36d29
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-03-27 21:18:47 +0100

    added database with meteodata

--- meteo_web/app/__init__.py
+++ meteo_web/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static_folder="assets")
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-from app import routes
+from app import routes, models
